2461887660324f2ad6b5f4bdcd06c489d7fa638f
gcarvalhomain <wm7j28bdfm@privaterelay.appleid.com>
2026-06-18 16:23:31 +0100

    Add crude

--- Models/Priorities.cs
+++ Models/Priorities.cs
@@ -1,6 +1,12 @@
+using System;
+
 namespace LoudApi.Models;
 
 public class Priorities
 {
-    
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public string? Description { get; set; }
+    public bool IsDone { get; set; }
+    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
 }
